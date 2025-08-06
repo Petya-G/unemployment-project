10c07f7c9a93c7bf70d029fe0122858276ec5658
Petya <vegvaripeter21@gmail.com>
2025-08-06 23:54:08 +0200

    added UnitView

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from tarfile import HeaderError
 from pygame.math import Vector2
 import pygame 
 from pygame.time import Clock
@@ -5,16 +6,19 @@ from pygame import MOUSEBUTTONDOWN, Surface
 import pygame_gui
 
 from command import MoveCommand
-from unit import Unit
+from unit import Unit, UnitView
 
 _ = pygame.init()
-manager = pygame_gui.UIManager((800, 600))
-screen: Surface = pygame.display.set_mode((1280, 720))
+WIDTH = 1280
+HEIGHT = 720
+manager = pygame_gui.UIManager((WIDTH, HEIGHT))
+screen: Surface = pygame.display.set_mode((WIDTH, HEIGHT))
 clock: Clock = pygame.time.Clock()
 running: bool = True
 dt: float = 0
 
-unit = Unit(pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2))
+unit = Unit(pygame.Vector2(WIDTH / 2, HEIGHT / 2), ('Enemy armor company'))
+unitView = UnitView(unit)
 
 while running:
     for event in pygame.event.get():
@@ -32,7 +36,7 @@ while running:
 
 
     _ = screen.fill("purple")
-    _ = pygame.draw.circle(screen, "red", unit.pos, 40)
+    screen.blit(unitView.surface, unitView.getPos())
 
     manager.draw_ui(screen)
     pygame.display.update()
--- unit.py
+++ unit.py
@@ -1,9 +1,31 @@
-from pygame import Vector2
+import io
+import pygame
+from pygame import Vector2, Surface
+import military_symbol 
 
 class Unit: 
     pos: Vector2
-    def __init__(self, pos: Vector2) -> None:
+    description: str
+
+    def __init__(self, pos: Vector2, description: str) -> None:
         self.pos = pos
+        self.description = description
         
     def move(self, new_pos: Vector2) -> None:
         self.pos = new_pos
+
+class UnitView:
+    unit: Unit
+    surface: Surface
+
+    def _getSurface(self) -> Surface:
+        print(self.unit.description)
+        svg: str = military_symbol.get_symbol_svg_string_from_name(self.unit.description)
+        return pygame.image.load(io.BytesIO(svg.encode()))
+
+    def __init__(self, unit: Unit) -> None:
+        self.unit = unit
+        self.surface = self._getSurface()
+    
+    def getPos(self) -> Vector2:
+        return Vector2(self.unit.pos.x - self.surface.width / 2, self.unit.pos.y - self.surface.height / 2)
